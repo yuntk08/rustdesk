8ea54014b0d7720ec5d8a231d4f71d2b4ba0da19
yuntk08 <yuntk08@qq.com>
2025-04-13 09:57:37 +0800

    修改子模块仓库

--- src/common.rs
+++ src/common.rs
@@ -944,7 +944,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rustdesk-api.noc-lucky.eu.org".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
